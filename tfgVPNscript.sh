2ca97279c852ce407ba22fef345070e90a326416
sk1ddie <sir@debian-BULLSEYE-live-builder-AMD64>
2022-04-10 14:25:23 +0200

    remove openvpn systemd

--- tfgVPNscript.sh
+++ tfgVPNscript.sh
@@ -1289,11 +1289,13 @@ function removeOpenVPN() {
 
 		if [[ $OS =~ (debian|ubuntu) ]]; then
 			apt-get remove --purge -y openvpn
+			find -type f -name "openvpn" --delete
+			find -type f -name "openvpn*service" -delete
 			if [[ -e /etc/apt/sources.list.d/openvpn.list ]]; then
 				rm /etc/apt/sources.list.d/openvpn.list
 				apt-get update
 			fi
-			find -type f -name "openvpn" -delete
+
 		elif [[ $OS == 'arch' ]]; then
 			pacman --noconfirm -R openvpn
 		elif [[ $OS =~ (centos|amzn|oracle) ]]; then
